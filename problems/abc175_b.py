39e55a29061f355565e58a33002271c03d26bb87
chokoryu <chokoryu@gmail.com>
2020-11-12 09:01:52 +0900

    20201112

--- problems/abc175_b.py
+++ problems/abc175_b.py
@@ -1,3 +1,4 @@
+import math
 from fractions import gcd
 from collections import Counter, deque, defaultdict
 from heapq import heappush, heappop, heappushpop, heapify, heapreplace, merge
@@ -8,7 +9,22 @@ def main():
     N = int(input())
     L = list(map(int, input().split()))
 
+    res = 0
 
+    for i in range(N):
+        for j in range(i, N):
+            for k in range(j, N):
+                if (L[i] == L[j]) or (L[j] == L[k]) or (L[i] == L[k]):
+                    continue
+                else:
+                    side_i = L[i] - L[j] - L[k]
+                    side_j = L[j] - L[i] - L[k]
+                    side_k = L[k] - L[i] - L[j]
+
+                    if (side_i < 0) and (side_j < 0) and (side_k < 0):
+                        res += 1
+
+    print(res)
 
 if __name__ == '__main__':
     main()
