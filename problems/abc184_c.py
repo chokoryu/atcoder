8e61a87c16081464066569172b644a7694d432ba
chokoryu <chokoryu@gmail.com>
2020-11-22 23:25:51 +0900

    20201122

--- problems/abc184_c.py
+++ problems/abc184_c.py
@@ -9,67 +9,19 @@ def main():
     r1, c1 = map(int, input().split())
     r2, c2 = map(int, input().split())
 
+    manhattan = abs(r1 - r2) + abs(c1 - c2)
+
     if r1 == r2 and c1 == c2:
         print(0)
-    elif (r1 + c1 == r2 + c2) or (r1 - c1 == r2 - c2) or (abs(r1 - r2) + abs(c1 - c2) <= 3):
+    elif (r1 + c1 == r2 + c2) or (r1 - c1 == r2 - c2) or (manhattan <= 3):
         print(1)
-    elif r2 - r1 > 0:
-        if c2 - c1 > 0:
-            # 2, x軸もy軸もプラス
-            d = r2 - r1 + c1
-            if abs(c2 - d) > 3:
-                if (abs(r1 - r2) + abs(c1 - c2)) % 2:
-                    print(3)
-                else:
-                    print(2)
-            else:
-                print(2)
-        elif c2 - c1 < 0:
-            # 1, x軸はプラス、y軸はマイナス
-            d = r1 + c1 - r2
-            if abs(d - c2) > 3:
-                if (abs(r1 - r2) + abs(c1 - c2)) % 2:
-                    print(3)
-                else:
-                    print(2)
-            else:
-                print(2)
-        else: # x軸に沿ってプラスに移動
-            if (abs(r1 - r2) + abs(c1 - c2)) % 2:
-                print(3)
-            else:
-                print(2)
-    elif r2 - r1 < 0:
-        if c2 - c1 > 0:
-            # 1, x軸はマイナス、y軸はプラス
-            d = r1 + c1 - r2
-            if abs(c2 - d) > 3:
-                if (abs(r1 - r2) + abs(c1 - c2)) % 2:
-                    print(3)
-                else:
-                    print(2)
-            else:
-                print(2)
-        elif c2 - c1 < 0:
-            # 2, x軸もy軸もマイナス
-            d = r2 - r1 + c1
-            if abs(c2 - d) >3:
-                if (abs(r1 - r2) + abs(c1 - c2)) % 2:
-                    print(3)
-                else:
-                    print(2)
-            else:
-                print(2)
-        else: # x軸に沿ってマイナスに移動
-            if (abs(r1 - r2) + abs(c1 - c2)) % 2:
-                print(3)
-            else:
-                print(2)
-    else: # x軸は移動無し
-        if (abs(r1 - r2) + abs(c1 - c2)) % 2:
+    elif abs(abs(c1 - c2) - abs(r1 - r2)) > 3:
+        if manhattan % 2:
             print(3)
         else:
             print(2)
+    else:
+        print(2)
 
 if __name__ == '__main__':
     main()
